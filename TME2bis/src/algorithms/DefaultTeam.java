67e8cc699c90df54a44d7f1b99e67d59d7924e5c
Ning GUO <ai535463858@gmail.com>
2019-10-02 13:43:59 +0200

    update

--- TME2bis/src/algorithms/DefaultTeam.java
+++ TME2bis/src/algorithms/DefaultTeam.java
@@ -1,64 +1,111 @@
 package algorithms;
 
 import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DefaultTeam {
 
+	public ArrayList<Point> expand(ArrayList<Point> origins,ArrayList<Point> subset, int edgeThreshold){
+		ArrayList<Point> setPlusUn = new ArrayList<Point>();
+		HashSet<Point> set = new HashSet<Point>();
+		set.addAll(subset);
+		for(Point p: subset) {
+			set.addAll(neighbor(p,origins,edgeThreshold));
+		}
+		setPlusUn.addAll(set);
+		return setPlusUn;
+	}
 
-	public ArrayList<Point> glouton(ArrayList<Point> points, int edgeThreshold) {
-
-		ArrayList<Point> dominant = new ArrayList<Point>();
+	public ArrayList<Point> algo(ArrayList<Point> points, int edgeThreshold, double a){
+		HashSet<Point> result = new HashSet<Point>();
 		ArrayList<Point> graphe = (ArrayList<Point>) points.clone();
-		while(!isValid(points,dominant,edgeThreshold)) {
-			Point pmax = getDegMax(graphe,edgeThreshold);
-			dominant.add(pmax);
-			ArrayList<Point> voisins = neighbor( pmax, graphe, edgeThreshold);
-			graphe.remove(pmax);
-			graphe.removeAll(voisins);
-		}
-		return dominant;
-	}
 
-	public Point getDegMax(ArrayList<Point> points,int edgeThreshold) {
-		int max = 0;
-		Point maxP = points.get(0);
-		for (Point p : points) {
-			int size = neighbor(p,points,edgeThreshold).size();
-			if (size>max) {
-				max = size;
-				maxP = p;
+		// get an arbitrary vertext v
+		int arbitrary ;
+		Point pointDepart ;
+		ArrayList<Point> subset = new ArrayList<Point>();
+		ArrayList<Point> dominant = new ArrayList<Point>();
+		int taille1 ;
+		int taille2 ;
+
+		while(!graphe.isEmpty()) {
+
+			// get an arbitrary vertex v
+			if(graphe.size()>2) {
+				arbitrary = graphe.size()/2;
+				pointDepart = graphe.get(arbitrary);
+			}else {
+				pointDepart = graphe.get(0);
 			}
+			//Initialiser le subset
+			subset = new ArrayList<Point>();
+			subset.add(pointDepart);
+			dominant.add(pointDepart);
+
+			subset = expand(graphe,subset,edgeThreshold);
+			dominant = getDominant(subset,edgeThreshold);
+			taille1 = dominant.size();
+			subset = expand(graphe,subset,edgeThreshold);
+			dominant = getDominant(subset,edgeThreshold);
+			taille2 = dominant.size();
+
+			while(taille2>taille1*a) {
+				taille1 = dominant.size();
+				subset = expand(graphe,subset,edgeThreshold);
+				dominant = getDominant(subset,edgeThreshold);
+				taille2 = dominant.size();
+			}
+			//union des dominant
+			result.addAll(dominant);
+			graphe.removeAll(subset);
 		}
-		return maxP;
+		ArrayList<Point> dominants = new ArrayList<Point>();
+		dominants.addAll(result);
+		dominants = cleanDominants(points,dominants,edgeThreshold);
+		return dominants;
+	}
+	
+	public ArrayList<Point> getDominant(ArrayList<Point> points, int edgeThreshold) {
+
+		ArrayList<Point> result = glouton(points, edgeThreshold);
+		int size1 = result.size();
+		result= localSearch2_1(points,result,edgeThreshold);
+		int size2 = result.size();
+		while(size2<size1) {
+			size1 = result.size();
+			result= localSearch2_1(points,result,edgeThreshold);
+			size2 = result.size();
+		}
+		return result;
 	}
 
-
+	public ArrayList<Point> calculDominatingSet(ArrayList<Point> points, int edgeThreshold) {
+		ArrayList<Point> result = algo(points,edgeThreshold,1.1);
+		return result;
+	}
+	
 	public ArrayList<Point> cleanDominants(ArrayList<Point> points, ArrayList<Point> domNaif, int edgeThreshold) {
 		ArrayList<Point> res = (ArrayList<Point>) domNaif.clone();
-
 		for (int i=0; i<res.size() ; i++) {
 			Point p = res.get(i);
 			res.remove(p);
 			if (!isValid(points, res, edgeThreshold))
 				res.add(p);
 		}
-
 		return res;
 	}
-
+	
 	public ArrayList<Point> localSearch2_1(ArrayList<Point> points, ArrayList<Point> domNaif, int edgeThreshold) {
 		ArrayList<Point> solutionPrime = new ArrayList<Point>(); 
-		System.out.println("dom size "+ domNaif.size());
 		for (int i=0; i<domNaif.size() ; i++) {
 			for (int j=i+1; j<domNaif.size();j++) {
 				Point m = domNaif.get(i);
@@ -76,46 +123,53 @@ public class DefaultTeam {
 					solutionPrime.remove(n);
 					solutionPrime.add(p);
 					if(isValid(points,solutionPrime,edgeThreshold)) return solutionPrime;
-
 				}
 			}
 		}
 		return domNaif;
 	}
 
-	public ArrayList<Point> calculDominatingSet(ArrayList<Point> points, int edgeThreshold) {
-
-		ArrayList<Point> result = glouton(points, edgeThreshold);
-		result= cleanDominants(points,result,edgeThreshold);
-
-		int nb1 = result.size();
-		result= localSearch2_1(points,result,edgeThreshold);
-		int nb2 = result.size();
-		while(nb2<nb1) {
-			nb1 = result.size();
-			result= localSearch2_1(points,result,edgeThreshold);
-			nb2 = result.size();
+	public ArrayList<Point> glouton(ArrayList<Point> points, int edgeThreshold) {
+		ArrayList<Point> dominant = new ArrayList<Point>();
+		ArrayList<Point> graphe = (ArrayList<Point>) points.clone();
+		while(!isValid(points,dominant,edgeThreshold)) {
+			Point pmax = getDegMax(graphe,edgeThreshold);
+			dominant.add(pmax);
+			ArrayList<Point> voisins = neighbor( pmax, graphe, edgeThreshold);
+			graphe.remove(pmax);
+			graphe.removeAll(voisins);
 		}
-		return result;
+		dominant= cleanDominants(points,dominant,edgeThreshold);
+		return dominant;
 	}
 
+	public Point getDegMax(ArrayList<Point> points,int edgeThreshold) {
+		int max = 0;
+		Point maxP = points.get(0);
+		for (Point p : points) {
+			int size = neighbor(p,points,edgeThreshold).size();
+			if (size>max) {
+				max = size;
+				maxP = p;
+			}
+		}
+		return maxP;
+	}
+	
 	public boolean isValid(ArrayList<Point> points, ArrayList<Point> dominants,
 			int edgeThreshold) {
-
-
 		ArrayList<Point> vertices = (ArrayList<Point>) points.clone();
 		Set<Point> voisins = new HashSet<Point>();
 		voisins.addAll(dominants);
 		for(Point p : dominants) {
-
 			voisins.addAll(neighbor(p,points,edgeThreshold));
 		}
 		vertices.removeAll(voisins);
 		return vertices.isEmpty();
 	}
+	
 	public ArrayList<Point> neighbor(Point p, ArrayList<Point> vertices, int edgeThreshold){
 		ArrayList<Point> result = new ArrayList<Point>();
-
 		for (Point point:vertices) {
 			if (point.distance(p)<edgeThreshold && !point.equals(p))
 				result.add((Point)point.clone());
@@ -150,7 +204,6 @@ public class DefaultTeam {
 			System.err.println("I/O exception: unable to create "+filename);
 		}
 	}
-
 	//FILE LOADER
 	private ArrayList<Point> readFromFile(String filename) {
 		String line;
